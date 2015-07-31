5f1db4633a5b6fc954e3bbb71f1dfd0795ae416a
liuwenbo@e.hunantv.com <liuwenbo@e.hunantv.com>
2015-07-31 18:09:42 +0800

    修复报错问题

---
+++ laboratory/src/main/java/com/wenbo/crypt/Shared.java
@@ -0,0 +1,8 @@
+package com.wenbo.crypt;
+
+public class Shared {
+
+	public static char pad;
+	
+	public static String chars;
+}
---
+++ laboratory/src/test/java/com/wenbo/laboratory/UserInfo.java
@@ -0,0 +1,74 @@
+package com.wenbo.laboratory;
+
+public class UserInfo {
+
+	private String avatar;
+	
+	private String email;
+	
+	private String isband;
+	
+	private String nickname;
+	
+	private String platform;
+	
+	private String reg_time;
+	
+	private String uuid;
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIsband() {
+		return isband;
+	}
+
+	public void setIsband(String isband) {
+		this.isband = isband;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+
+	public String getReg_time() {
+		return reg_time;
+	}
+
+	public void setReg_time(String reg_time) {
+		this.reg_time = reg_time;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+}
